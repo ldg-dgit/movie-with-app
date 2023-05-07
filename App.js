c19a2ee7f23197193929b6c02f53a193d9cbfe70
Bobby Lee <ldg@dgit.co>
2023-05-07 23:23:15 +0900

    Tab navigation

--- App.js
+++ App.js
@@ -3,33 +3,58 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset, useAssets } from "expo-asset";
+import { Asset } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "./navigation/Tabs";
 
 SplashScreen.preventAutoHideAsync();
 
-export default function App() {
-	const [assets] = useAssets([
-		require("./asset/GithubContributionsSticker.png"),
-		"https://cdn.openai.com/API/images/gradient_card_1.png",
-	]);
-	const [fonts] = Font.useFonts(Ionicons.font);
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
-	const onLayoutRootView = useCallback(async () => {
-		if (assets || fonts) {
-			await SplashScreen.hideAsync();
+const loadImages = (images) =>
+	images.map((image) => {
+		if (typeof image === "string") {
+			return Image.prefetch(image);
+		} else {
+			return Asset.loadAsync(image);
 		}
 	});
 
-	if (!assets) {
+export default function App() {
+	const [ready, setReady] = useState(false);
+	useEffect(() => {
+		async function prepare() {
+			try {
+				const fonts = loadFonts([Ionicons.font]);
+				const images = loadImages([
+					require("./asset/GithubContributionsSticker.png"),
+					"https://cdn.openai.com/API/images/gradient_card_1.png",
+				]);
+				await Promise.all([...fonts, ...images]);
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				setReady(true);
+			}
+		}
+		prepare();
+	}, []);
+
+	const onLayoutRootView = useCallback(async () => {
+		if (ready) {
+			await SplashScreen.hideAsync();
+		}
+	}, [ready]);
+
+	if (!ready) {
 		return null;
 	}
 
 	return (
-		<View
-			style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-			onLayout={onLayoutRootView}
-		>
-			<Text>Splash Screen Demo! 👋</Text>
-		</View>
+		// <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+		<NavigationContainer onReady={onLayoutRootView}>
+			<Tabs />
+		</NavigationContainer>
+		// </View>
 	);
 }
