7a78ff234f0e6de4f40de87cef0186e9aec56403
Bobby Lee <ldg@dgit.co>
2023-05-07 14:56:55 +0900

    preload asset by Hook

--- App.js
+++ App.js
@@ -3,48 +3,24 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 
 SplashScreen.preventAutoHideAsync();
 
-const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+export default function App() {
+	const [assets] = useAssets([
+		require("./asset/GithubContributionsSticker.png"),
+		"https://cdn.openai.com/API/images/gradient_card_1.png",
+	]);
+	const [fonts] = Font.useFonts(Ionicons.font);
 
-const loadImages = (images) =>
-	images.map((image) => {
-		if (typeof image === "string") {
-			return Image.prefetch(image);
-		} else {
-			return Asset.loadAsync(image);
+	const onLayoutRootView = useCallback(async () => {
+		if (assets || fonts) {
+			await SplashScreen.hideAsync();
 		}
 	});
 
-export default function App() {
-	const [ready, setReady] = useState(false);
-	useEffect(() => {
-		async function prepare() {
-			try {
-				const fonts = loadFonts([Ionicons.font]);
-				const images = loadImages([
-					require("./asset/GithubContributionsSticker.png"),
-					"https://cdn.openai.com/API/images/gradient_card_1.png",
-				]);
-				await Promise.all([...fonts, ...images]);
-			} catch (e) {
-				console.warn(e);
-			} finally {
-				setReady(true);
-			}
-		}
-		prepare();
-	}, []);
-
-	const onLayoutRootView = useCallback(async () => {
-		if (ready) {
-			await SplashScreen.hideAsync();
-		}
-	}, [ready]);
-
-	if (!ready) {
+	if (!assets) {
 		return null;
 	}
 
