dbea7e7466cf42e48cec996d2de6fe9727353197
Bobby Lee <ldg@dgit.co>
2023-05-08 10:27:22 +0900

    stack nav config test

---
+++ navigation/Stack.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Text, View, TouchableOpacity } from "react-native";
+
+const ScreenOne = ({ navigation: { navigate } }) => (
+	<TouchableOpacity onPress={() => navigate("Two")}>
+		<Text>go to two</Text>
+	</TouchableOpacity>
+);
+const ScreenTwo = ({ navigation: { navigate } }) => (
+	<TouchableOpacity onPress={() => navigate("Three")}>
+		<Text>go to three</Text>
+	</TouchableOpacity>
+);
+const ScreenThree = ({ navigation: { setOptions } }) => (
+	<TouchableOpacity onPress={() => setOptions({ title: "changed" })}>
+		<Text>change title</Text>
+	</TouchableOpacity>
+);
+
+const NativeStack = createNativeStackNavigator();
+
+const Stack = () => (
+	<NativeStack.Navigator screenOptions={{ headerBackTitleVisible: false }}>
+		<NativeStack.Screen
+			name="One"
+			component={ScreenOne}
+			options={{ title: "1" }}
+		/>
+		<NativeStack.Screen name="Two" component={ScreenTwo} />
+		<NativeStack.Screen
+			name="Three"
+			component={ScreenThree}
+			options={{ presentation: "modal" }}
+		/>
+	</NativeStack.Navigator>
+);
+
+export default Stack;
