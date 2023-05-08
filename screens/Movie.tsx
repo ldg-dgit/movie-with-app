7ed3e06fff51ba2791c71deb3d807eb86924347e
Bobby Lee <ldg@dgit.co>
2023-05-08 16:54:44 +0900

    refactoring now playing slider

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,24 +1,14 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import {
-	ActivityIndicator,
-	Dimensions,
-	StyleSheet,
-	useColorScheme,
-} from "react-native";
+import { ActivityIndicator, Dimensions } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
-import { makeImgPath } from "../utils";
-import { BlurView } from "expo-blur";
+import Slide from "../components/Slide";
 
 const API_KEY = "fea2b17df637d4f3a55228d4c0ef6c3d";
 
 const Container = styled.ScrollView`
 	background-color: ${(props) => props.theme.mainBgColor};
-`;
-
-const View = styled.View`
-	flex: 1;
 `;
 
 const Loader = styled.View`
@@ -27,46 +17,9 @@ const Loader = styled.View`
 	align-items: center;
 `;
 
-const BgImg = styled.Image``;
-
-const Poster = styled.Image`
-	width: 100px;
-	height: 160px;
-	border-radius: 5px;
-`;
-
-const Title = styled.Text`
-	font-size: 16px;
-	font-weight: 600;
-	color: white;
-`;
-
-const Wrapper = styled.View`
-	flex-direction: row;
-	height: 100%;
-	justify-content: center;
-	align-items: center;
-`;
-
-const Column = styled.View`
-	width: 50%;
-	margin-left: 15px;
-`;
-
-const Overview = styled.Text`
-	color: rgba(255, 255, 255, 0.8);
-	margin-top: 10px;
-	font-size: 13px;
-`;
-
-const Votes = styled(Overview)`
-	font-size: 14px;
-`;
-
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
-	const isDark = useColorScheme() === "dark";
 	const [loading, setLoading] = useState(true);
 	const [nowPlaying, setNowPlaying] = useState([]);
 	const getNowPlaying = async () => {
@@ -97,28 +50,14 @@ const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
 				containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
 			>
 				{nowPlaying.map((movie) => (
-					<View key={movie.id}>
-						<BgImg
-							source={{ uri: makeImgPath(movie.backdrop_path) }}
-							style={StyleSheet.absoluteFill}
-						></BgImg>
-						<BlurView
-							style={StyleSheet.absoluteFill}
-							intensity={50}
-							tint={isDark ? "dark" : "light"}
-						>
-							<Wrapper>
-								<Poster source={{ uri: makeImgPath(movie.poster_path) }} />
-								<Column>
-									<Title>{movie.original_title}</Title>
-									{movie.vote_average > 0 ? (
-										<Votes>{movie.vote_average} / 10 ⭐️</Votes>
-									) : null}
-									<Overview>{movie.overview.slice(0, 100)}...</Overview>
-								</Column>
-							</Wrapper>
-						</BlurView>
-					</View>
+					<Slide
+						key={movie.id}
+						backdrop_path={movie.backdrop_path}
+						poster_path={movie.poster_path}
+						original_title={movie.original_title}
+						vote_average={movie.vote_average}
+						overview={movie.overview}
+					/>
 				))}
 			</Swiper>
 		</Container>
