b1422adfd9a312ac0d3372e514997e84991a6d47
Bobby Lee <ldg@dgit.co>
2023-05-08 17:02:35 +0900

    get trending and upcoming data

--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -22,6 +22,24 @@ const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
 	const [loading, setLoading] = useState(true);
 	const [nowPlaying, setNowPlaying] = useState([]);
+	const [upcoming, setUpcoming] = useState([]);
+	const [trending, setTrending] = useState([]);
+	const getTrending = async () => {
+		const { results } = await (
+			await fetch(
+				`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+			)
+		).json();
+		setTrending(results);
+	};
+	const getUpcoming = async () => {
+		const { results } = await (
+			await fetch(
+				`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+			)
+		).json();
+		setUpcoming(results);
+	};
 	const getNowPlaying = async () => {
 		const { results } = await (
 			await fetch(
@@ -29,10 +47,13 @@ const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
 			)
 		).json();
 		setNowPlaying(results);
+	};
+	const getData = async () => {
+		await Promise.all([getTrending, getUpcoming, getNowPlaying]);
 		setLoading(false);
 	};
 	useEffect(() => {
-		getNowPlaying();
+		getData();
 	}, []);
 	return loading ? (
 		<Loader>
