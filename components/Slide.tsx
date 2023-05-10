84b7c40196f15ac961d6c6a8474ddf3bed556012
Bobby Lee <ldg@dgit.co>
2023-05-10 13:59:57 +0900

    trending movies

---
+++ components/Slide.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { StyleSheet, View, useColorScheme } from "react-native";
+import styled from "styled-components/native";
+import { makeImgPath } from "../utils";
+import { BlurView } from "expo-blur";
+import Poster from "./Poster";
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text`
+	font-size: 16px;
+	font-weight: 600;
+	color: white;
+`;
+
+const Wrapper = styled.View`
+	flex-direction: row;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Column = styled.View`
+	width: 50%;
+	margin-left: 15px;
+`;
+
+const Overview = styled.Text`
+	color: rgba(255, 255, 255, 0.8);
+	margin-top: 10px;
+	font-size: 13px;
+`;
+
+const Votes = styled(Overview)`
+	font-size: 14px;
+`;
+
+interface SlideProps {
+	backdrop_path: string;
+	poster_path: string;
+	original_title: string;
+	vote_average: number;
+	overview: string;
+}
+
+const Slide: React.FC<SlideProps> = ({
+	backdrop_path,
+	poster_path,
+	original_title,
+	vote_average,
+	overview,
+}) => {
+	const isDark = useColorScheme() === "dark";
+	return (
+		<View style={{ flex: 1 }}>
+			<BgImg
+				source={{ uri: makeImgPath(backdrop_path) }}
+				style={StyleSheet.absoluteFill}
+			></BgImg>
+			<BlurView
+				style={StyleSheet.absoluteFill}
+				intensity={50}
+				tint={isDark ? "dark" : "light"}
+			>
+				<Wrapper>
+					<Poster path={poster_path} />
+					<Column>
+						<Title>{original_title}</Title>
+						{vote_average > 0 ? <Votes>{vote_average} / 10 ⭐️</Votes> : null}
+						<Overview>{overview.slice(0, 100)}...</Overview>
+					</Column>
+				</Wrapper>
+			</BlurView>
+		</View>
+	);
+};
+
+export default Slide;
--- screens/Movie.tsx
+++ screens/Movie.tsx
@@ -1,9 +1,10 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, Dimensions } from "react-native";
+import { ActivityIndicator, Dimensions, ScrollView } from "react-native";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
 import Slide from "../components/Slide";
+import Poster from "../components/Poster";
 
 const API_KEY = "fea2b17df637d4f3a55228d4c0ef6c3d";
 
@@ -19,7 +20,34 @@ const Loader = styled.View`
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
+const ListTitle = styled.Text`
+	color: white;
+	font-size: 18px;
+	font-weight: 600;
+	margin-left: 30px;
+`;
+
+const TrendingScroll = styled.ScrollView`
+	margin-top: 20px;
+`;
+
+const Movie = styled.View`
+	margin-right: 20px;
+	align-items: center;
+`;
+
+const Title = styled.Text`
+	color: white;
+	font-weight: 600;
+	margin-top: 7px;
+	margin-bottom: 5px;
+`;
+const Votes = styled.Text`
+	color: rgba(255, 255, 255, 0.8);
+	font-size: 10px;
+`;
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
 	const [loading, setLoading] = useState(true);
 	const [nowPlaying, setNowPlaying] = useState([]);
 	const [upcoming, setUpcoming] = useState([]);
@@ -49,7 +77,7 @@ const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
 		setNowPlaying(results);
 	};
 	const getData = async () => {
-		await Promise.all([getTrending, getUpcoming, getNowPlaying]);
+		await Promise.all([getTrending(), getUpcoming(), getNowPlaying()]);
 		setLoading(false);
 	};
 	useEffect(() => {
@@ -68,7 +96,11 @@ const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
 				autoplayTimeout={3.5}
 				showsPagination={false}
 				showsButtons={false}
-				containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+				containerStyle={{
+					width: "100%",
+					height: SCREEN_HEIGHT / 4,
+					marginBottom: 30,
+				}}
 			>
 				{nowPlaying.map((movie) => (
 					<Slide
@@ -81,8 +113,25 @@ const Movie: React.FC<NativeStackScreenProps<any, "Movie">> = ({}) => {
 					/>
 				))}
 			</Swiper>
+			<ListTitle>Trending Movies</ListTitle>
+			<TrendingScroll
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				contentContainerStyle={{ paddingLeft: 30 }}
+			>
+				{trending.map((movie) => (
+					<Movie key={movie.id}>
+						<Poster path={movie.poster_path} />
+						<Title>
+							{movie.original_title.slice(0, 13)}
+							{movie.original_title.length > 13 ? "..." : null}
+						</Title>
+						<Votes>{movie.vote_average} / 10</Votes>
+					</Movie>
+				))}
+			</TrendingScroll>
 		</Container>
 	);
 };
 
-export default Movie;
+export default Movies;
